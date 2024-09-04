8b03290aba0e777f458bdcd18b7239e007239059
Fachep <mail@fachep.com>
2024-09-05 04:49:13 +0800

    fix: cyclic reference in timer tickers

--- src/factory/timers.rs
+++ src/factory/timers.rs
@@ -1,5 +1,4 @@
 use crate::{State, TickerFactory};
-use std::mem::MaybeUninit;
 use std::rc::Rc;
 use wasm_bindgen::__rt::WasmRefCell;
 use wasm_bindgen::prelude::*;
@@ -19,34 +18,31 @@ macro_rules! timer_ticker_factory_impl {
         fn new(mut task: impl FnMut() + 'static) -> Result<Self::Output, JsValue> {
             let state = Rc::new(WasmRefCell::new(State::Stopped));
             let token = Rc::new(WasmRefCell::new(None));
-            let cb_uninit: Rc<Closure<dyn FnMut()>> = Rc::new(unsafe {
-                #[allow(invalid_value)]
-                MaybeUninit::uninit().assume_init()
-            });
 
             let state_ = state.clone();
             let token_ = token.clone();
-            let cb_uninit_ = cb_uninit.clone();
-            let cb: Closure<dyn FnMut()> = Closure::new(move || {
-                if state_.borrow().eq(&State::Started) {
-                    task();
-                    match $FTimer(cb_uninit_.as_ref().as_ref().unchecked_ref()) {
-                        Ok(token) => {
-                            token_.borrow_mut().replace(token);
-                        },
-                        Err(e) => *state_.borrow_mut() = State::Error(e),
-                    };
-                }
+            let cb = Rc::new_cyclic(move |weak| {
+                let weak = weak.clone();
+                Closure::new(move || {
+                    if state_.borrow().eq(&State::Started) {
+                        task();
+                        if let Some(cb) = weak.upgrade() {
+                            match $FTimer(AsRef::<Closure<dyn FnMut()>>::as_ref(&cb).as_ref().unchecked_ref()) {
+                                Ok(token) => {
+                                    token_.borrow_mut().replace(token);
+                                },
+                                Err(e) => {
+                                    *state_.borrow_mut() = State::Error(e);
+                                }
+                            }
+                        }
+                    }
+                })
             });
-            let _ = MaybeUninit::new(std::mem::replace(
-                #[allow(invalid_reference_casting)]
-                unsafe {&mut *(&*cb_uninit as *const Closure<dyn FnMut()>).cast_mut()},
-                cb
-            ));
             Ok(Self::Output {
                 token,
                 state,
-                cb: cb_uninit,
+                cb,
             })
         }
     }
